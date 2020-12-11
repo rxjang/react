cc3bbe0e5b26fc03297594a25444b66ee49949dc
rxjang <rxforp@naver.com>
2020-12-11 22:46:56 +0900

    jsx

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import Hello from './Hello';
+import './App.css';
 
 function App() {
+  const name='react';
+  const style={
+    backgroundColor:'black',
+    color:'aqua',
+    fontSize:24,
+    padding:'1rem'
+  }
   return (
-    <div>
-      <Hello/>
-      <Hello/>
-      <Hello/>
-    </div>
+    <> 
+    {/* 
+      jsx 문법
+      - return 문에는 반드시 하나의 최상위 태그가 있어야 함 ->fragments tag사용:<Fragment> or <>
+    */}
+        <Hello
+          //태그 내 주석 쓰는 법
+        />
+        <div style={style}>{name}</div>
+        <div className="gray-box"></div>
+      </>
+
   );
 }
 

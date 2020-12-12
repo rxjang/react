e0e529e66ecb10cfde886985ef073bea9a35d93c
rxjang <rxforp@naver.com>
2020-12-12 09:02:08 +0900

    props

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,28 +1,14 @@
 import React from 'react';
 import Hello from './Hello';
-import './App.css';
+import Wrapper from './Wrapper';
 
 function App() {
-  const name='react';
-  const style={
-    backgroundColor:'black',
-    color:'aqua',
-    fontSize:24,
-    padding:'1rem'
-  }
+  
   return (
-    <> 
-    {/* 
-      jsx 문법
-      - return 문에는 반드시 하나의 최상위 태그가 있어야 함 ->fragments tag사용:<Fragment> or <>
-    */}
-        <Hello
-          //태그 내 주석 쓰는 법
-        />
-        <div style={style}>{name}</div>
-        <div className="gray-box"></div>
-      </>
-
+    <Wrapper>
+      <Hello name="react" color="red"></Hello>
+      <Hello color="pink"></Hello>
+    </Wrapper>
   );
 }
 

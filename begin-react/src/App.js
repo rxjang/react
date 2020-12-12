4d884f11a8380b9dce3fbe95cd87ce7a8a320605
rxjang <rxforp@naver.com>
2020-12-12 09:10:08 +0900

    Conditional Rendering

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -6,7 +6,7 @@ function App() {
   
   return (
     <Wrapper>
-      <Hello name="react" color="red"></Hello>
+      <Hello name="react" color="red" isSpecial={true}></Hello>
       <Hello color="pink"></Hello>
     </Wrapper>
   );

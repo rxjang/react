20e80ac2daf968df6492dc33a8550ca671f9492f
rxjang <rxforp@naver.com>
2020-12-12 09:23:21 +0900

    useState

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import Hello from './Hello';
-import Wrapper from './Wrapper';
+import Counter from './Counter';
 
 function App() {
   
   return (
-    <Wrapper>
-      <Hello name="react" color="red" isSpecial={true}></Hello>
-      <Hello color="pink"></Hello>
-    </Wrapper>
+    <Counter/>
   );
 }
 

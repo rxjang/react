4878ac2f34d7a558d8446aa827d0ae61e329061c
rxjang <rxforp@naver.com>
2020-12-13 19:53:40 +0900

    useRef

--- begin-react/src/inputSample.js
+++ begin-react/src/inputSample.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState,useRef} from 'react';
 
 function InputSample(){
     const [inputs,setInputs]=useState({
@@ -6,10 +6,11 @@ function InputSample(){
         nickname:'',
     });
 
+    const nameInput=useRef();
     const {name,nickname}=inputs;
 
     const onChange=(e)=>{
-        const {name,value}=inputs;
+        const {name,value}=e.target;
 
         setInputs({
             ...inputs,
@@ -21,11 +22,12 @@ function InputSample(){
         setInputs({
             name:'',
             nickname:'',
-        })
+        });
+        nameInput.current.focus();
     };
     return (
         <div>
-            <input name="name" placeholder="이름" onChange={onChange} value={name}/>
+            <input name="name" placeholder="이름" onChange={onChange} value={name} ref={nameInput}/>
             <input name="nickname" placeholder="닉네임" onChange={onChange} value={nickname}/>
             <button onClick={onReset}>초기화</button>
             <div>

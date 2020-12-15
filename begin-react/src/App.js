f8cabff5144baba9d9170176a962bbe5390c1253
rxjang <rxforp@naver.com>
2020-12-15 15:50:47 +0900

    immer library

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,7 +1,7 @@
 import React, {useReducer, useRef, useMemo, useCallback, createContext} from 'react';
 import CreateUser from './CreateUser';
+import produce from 'immer';
 import UserList from './UserList';
-import useInputs from './useInputs';
 
 function countActiveUsers(users){
   console.log('활성 사용자 수를 세는 중...');
@@ -34,23 +34,32 @@ const initialState={
 function reducer(state,action){
   switch (action.type){
       case 'CREATE_USER':
-        return{
-          users:state.users.concat(action.user)
-        }
+        return produce(state, draft => {
+          draft.users.push(action.user);
+        });
+        // return{
+        //   users:state.users.concat(action.user)
+        // }
       case 'TOGGLE_USER':
-        return{
-          ...state,
-          users: state.users.map(user =>
-            user.id===action.id
-              ? {...user,active: !user.active}
-              : user
-            )
-        }
+        return produce(state,draft =>{
+          const user = draft.users.find(user => user.id ===action.id);
+          user.active=!user.active;
+        });
+        // return{
+        //   users: state.users.map(user =>
+        //     user.id===action.id
+        //       ? {...user,active: !user.active}
+        //       : user
+        //     )
+        // }
       case 'REMOVE_USER':
-        return{
-          ...state,
-          users: state.users.filter(user => user.id !==action.id)
-        }
+        return produce(state,draft => {
+          const index = draft.users.findIndex(user => user.id ===action.id);
+          draft.users.splice(index,1);
+        });
+        // return{
+        //   users: state.users.filter(user => user.id !==action.id)
+        // }
     default:
       throw new Error('Unhandeled action');
   }

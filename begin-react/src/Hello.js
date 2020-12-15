3d8bc10c1324e9c5c93e4e953a9a87f6b3dc5d44
rxjang <rxforp@naver.com>
2020-12-15 17:00:46 +0900

    class component

--- begin-react/src/Hello.js
+++ begin-react/src/Hello.js
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-function Hello({color,name, isSpecial}){
-    return (
-        <div style={{
-            color
-        }}>
-            {/* {isSpecial && <b>*</b>} */}
-            <b>{isSpecial? 'special':'not special'}</b>
-            안녕하세요 {name}
-        </div>
-    );
+class Hello extends Component{
+    static dafaultProps = {
+        name:'이름없음',
+    };
+    render(){
+        const {color,isSpecial,name} = this.props;
+        return (
+            <div style={{color}}>
+                {isSpecial && <b>*</b>}
+                안녕하세여 {name}
+            </div>
+        )
+    }
 }
 
-Hello.defaultProps={
-    name:'이름 없음'
-};
+// function Hello({color,name, isSpecial}){
+//     return (
+//         <div style={{
+//             color
+//         }}>
+//             {/* {isSpecial && <b>*</b>} */}
+//             <b>{isSpecial? 'special':'not special'}</b>
+//             안녕하세요 {name}
+//         </div>
+//     );
+// }
+
+// Hello.defaultProps={
+//     name:'이름 없음'
+// };
+
 export default Hello;

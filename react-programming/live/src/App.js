5dbcbad01f4ec8082052a759b332b71b2f771324
장서현 <rxforp@naver.com>
2021-04-09 17:06:19 +0900

    Component Function return value

--- react-programming/live/src/App.js
+++ react-programming/live/src/App.js
@@ -1,42 +1,18 @@
-import React, { useState } from 'react';
-import ColorPage from './ColorPage';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Counter from './Counter';
 
 export default function App() {
-    const [todoList, setTodoList] = useState([]);
-    const [currentId, setCurrentId] = useState(1);
-    const [desc, setDesc] = useState('');
-    const [showOdd, setShowOdd] = useState(false);
-    function onAdd() {
-        const todo = {id: currentId, desc};
-        setCurrentId(currentId + 1);
-        setTodoList([...todoList, todo]);
-    }
-    function onDelete(e) {
-        const id = Number(e.target.dataset.id)
-        const newTodoList = todoList.filter(todo => todo.id !== id)
-        setTodoList(newTodoList);
-    }
-    function onSaveToServer() {}
     return (
-        <div>
-            <h3>할 일 목록</h3>
-            <ul>
-                {todoList
-                .filter((_, index) => (showOdd ? index % 2 === 0 : true))
-                .map(todo => (
-                    <li key={todo.id}>
-                        <span>{todo.desc}</span>
-                        <button data-id={todo.id} onClick={onDelete}>
-                            삭제
-                        </button>
-                    </li>
-                ))}
-            </ul>
-            <input type="text" value = {desc} onChange={e =>setDesc(e.target.value)} />
-            <button onClick={onAdd}>추가</button>
-            <button onClick={() => {setShowOdd(!showOdd)}}>홀수 아이템만 보기 on/off</button>
-            <button onClick={onSaveToServer}>서버에 저장</button>
-            <ColorPage/>
-        </div>
+    <>
+        <p>안녕</p>
+        <Counter />
+        {ReactDOM.createPortal(
+            <div>
+                <p>안녕하세요</p>
+            </div>,
+            document.getElementById('something'),
+        )}
+    </>
     );
 }

07ec180ae2c614cf66465eebbd6ebe7a2d820319
장서현 <rxforp@naver.com>
2021-04-11 13:33:40 +0900

    game 2048

---
+++ game2048/src/App.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react';
+import AboveGame from './component/AboveGame';
+import Game from './component/Game';
+import Header from './component/Header';
+import useLocalStorageNumber from './hook/useLocalStorage';
+
+export default function App() {
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useLocalStorageNumber('bestScore', 0);
+
+  useEffect(() => {
+    if(score > bestScore) {
+      setBestScore(score);
+    }
+  });
+
+  return (
+    <div className="container">
+      <Header score={score} bestScore={bestScore}/>
+      <AboveGame />
+      <Game setScore={setScore}/>
+    </div>
+  );
+}
---
+++ react-programming/2-virtual-dom/6-Todo.js
@@ -0,0 +1,24 @@
+function Todo({ title, desc }) {
+    const [priority, setPriority] = useState('high');
+    function onClick() {
+        setPriority(priority == 'high' ? 'low' : 'high');
+    }
+
+    return (
+        <div>
+            <Title title = {title} />
+            <p>{desc}</p>
+            <p>{priority === 'high' ? '우선순위 높음' : '우선순위 낮음'}</p>
+            <button onClick={onclick}>우선순위 변경</button>
+        </div>
+    );
+}
+
+const Title = React.memo(({title}) => {
+    return <p style={{ color: 'blue'}}>{title}</p>;
+});
+
+ReactDom.render(
+    <Todo title = "리액트 공부하기" desc="실전 리액트 프로그래밍을 공부한다" />,
+    document.getElementById('root'),
+);

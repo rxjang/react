20ff92e9207afb16783de3f2dfa0a9852b18dd7f
장서현 <rxforp@naver.com>
2021-05-08 22:43:49 +0900

    event function

---
+++ redux-codeevolution/hello-world/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import FunctionClick from "./components/FunctionClick";
+
+function App() {
+  return (
+    <div className="App">
+      <FunctionClick />
+    </div>
+  );
+}
+
+export default App;

d81a5cdc82541338ab3ed1268e974cd4fbb3bfc3
장서현 <rxforp@naver.com>
2021-05-11 08:49:19 +0900

    useContext:

--- redux-codeevolution/state-hook/src/App.js
+++ redux-codeevolution/state-hook/src/App.js
@@ -1,10 +1,18 @@
+import React from "react";
 import "./App.css";
-import DataFetching from "./components/DataFetching";
+import ComponentC from "./components/ComponentC";
+
+export const UserContext = React.createContext();
+export const ChannelContext = React.createContext();
 
 function App() {
   return (
     <div className="App">
-      <DataFetching />
+      <UserContext.Provider value={"Felix"}>
+        <ChannelContext.Provider value={"Stay"}>
+          <ComponentC />
+        </ChannelContext.Provider>
+      </UserContext.Provider>
     </div>
   );
 }

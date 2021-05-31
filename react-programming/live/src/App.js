394c226a9419a3cbbfedc749aa1b77cc92f83904
장서현 <rxforp@naver.com>
2021-05-31 23:31:54 +0900

    react-redux

--- react-programming/live/src/App.js
+++ react-programming/live/src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import FriendMain from "./friend/container/FriendMain";
 import TimelineMain from "./timeline/container/TimelineMain";
+import { Provider } from "react-redux";
+import store from "./common/store";
 
 export default function App() {
   return (
-    <div>
-      <FriendMain />
-      <TimelineMain />
-    </div>
+    <Provider store={store}>
+      <div>
+        <FriendMain />
+        <TimelineMain />
+      </div>
+    </Provider>
   );
 }
--- react-programming/live/src/friend/container/FriendMain.js
+++ react-programming/live/src/friend/container/FriendMain.js
@@ -1,28 +1,17 @@
-import React, { useEffect, useReducer } from "react";
+import React from "react";
 import store from "../../common/store";
 import { getNextFriends } from "../../common/mockData";
 import FriendList from "../component/FriendList";
 import { addFriend } from "../state";
+import { useSelector } from "react-redux";
 
 export default function FriendMain() {
-  const [, foreceUpdate] = useReducer((v) => v + 1, 0);
-  useEffect(() => {
-    let prevFriends = store.getState().friend.friends;
-    const unsubscirbe = store.subscribe(() => {
-      const friends = store.getState().friend.friends;
-      if (prevFriends !== friends) {
-        foreceUpdate();
-      }
-      prevFriends = friends;
-    });
-    return () => unsubscirbe();
-  }, []);
+  const friends = useSelector((state) => state.friend.friends);
   function onAdd() {
     const friend = getNextFriends();
     store.dispatch(addFriend(friend));
   }
   console.log("FriendMain render");
-  const friends = store.getState().friend.friends;
   return (
     <div>
       <button onClick={onAdd}>Add friend</button>

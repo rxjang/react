80f03bf8fa5189af434ef675bae68731312a3952
장서현 <rxforp@naver.com>
2021-06-09 22:45:26 +0900

    user's page

--- whois/src/App.js
+++ whois/src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
 import Search from "./search/container/Search";
 import "antd/dist/antd.css";
+import { Route } from "react-router";
+import User from "./user/conatiner/User";
 
 export default function App() {
-  return <Search />;
+  return (
+    <>
+      <Route exact path="/" component={Search} />
+      <Route path="/user/:name" component={User} />
+    </>
+  );
 }
